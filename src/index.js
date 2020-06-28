6a629fa0d72af7e326eb00bc2997d722853fed7b
Carlos Cortes <krlozadan@gmail.com>
2020-06-28 12:46:30 -0700

    #12 Remove the deprecated babel polyfill and update the dev dependencies

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import '@babel/polyfill'
-
 module.exports = (promise) => {
   if (Array.isArray(promise)) {
     return Promise.all(promise).then(data => ({ data, error: null })).catch(error => ({ data: null, error }))
